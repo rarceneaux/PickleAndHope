48eb6b7225eb247a15131038ebaeeaef5d8cc993
Raymond <raymond.techcoder@gmail.com>
2020-04-04 11:27:37 -0500

    Add project files.

---
+++ Controllers/PickleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PickleAndHope.Models;
+using PickleAndHope.DataAccess;
+
+namespace PickleAndHope.Controllers
+{
+    [Route("api/pickles")]
+    [ApiController]
+    public class PickleController : ControllerBase
+    {
+        PickleRepository _repository = new PickleRepository();
+        [HttpPost] //Attribute//
+        public IActionResult AddPickle(Pickle pickleToAdd)
+        {
+            var existingPickle = _repository.GetByType(pickleToAdd.Type);
+            if (existingPickle == null)
+            {
+                _repository.Add(pickleToAdd);
+                return Created("", pickleToAdd);
+            }
+            else
+            {
+                _repository.Update(pickleToAdd);
+                var updatedPickle = _repository.GetByType(pickleToAdd.Type);
+                return Ok(updatedPickle);
+            }
+        }
+    }
+}
---
+++ DataAccess/PickleRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PickleAndHope.Models;
+
+namespace PickleAndHope.DataAccess
+{
+    
+    public class PickleRepository
+    {
+        static List<Pickle> _pickles = new List<Pickle> { new Pickle { Type = "Bread and Butter", NumberInStock = 5 } };
+        public void Add(Pickle pickle)
+        {
+            _pickles.Add(pickle);
+        }
+
+        public void Remove(string type)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public Pickle Update(Pickle pickle)
+        {
+            var pickleToUpdate = GetByType(pickle.Type);
+            pickleToUpdate.NumberInStock += pickle.NumberInStock;
+            return pickleToUpdate;
+        }
+
+        public Pickle GetByType(string type)
+        {
+          return  _pickles.FirstOrDefault(p => p.Type == type);
+        }
+
+        public List<Pickle> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
